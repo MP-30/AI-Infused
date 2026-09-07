f5073174c1ebde471131782fbe4c375d6094da5d
mp-30 <adityabhadauriya93@gmail.com>
2026-09-07 13:54:03 +0530

    scrapper done

---
+++ 01_scraper/app.py
@@ -0,0 +1,9 @@
+import gradio as gr
+from summarizer import summarize
+
+gr.Interface(
+    fn=summarize,                                  # your function
+    inputs=gr.Textbox(label="Website URL"),
+    outputs=gr.Markdown(label="Summary"),
+    title="ðŸ”Ž AI Website Summarizer",
+).launch(share=True)   # share=True â†’ a public link you can post! ðŸŽ‰
